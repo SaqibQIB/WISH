34d6fce605a69e584a8e26a044ca2fdb79c4ef00
msaqib <msaqib.ems@gmail.com>
2025-08-02 10:33:19 -0700

    com

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+    // next.config.mjs
+    /** @type {import('next').NextConfig} */
+    const nextConfig = {
+      output: "export", // Enables static HTML export
+      // basePath: "/your-repo-name", // Uncomment and set if needed
+      reactStrictMode: true,
+    };
+
+    export default nextConfig;
